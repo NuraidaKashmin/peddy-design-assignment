57e2f9f9d36f9bbbb7685696f5751e3c97cc2766
NuraidaKashmin <nuraidakkashmin@gmail.com>
2024-10-07 04:36:44 +0600

    modals are dynamic now

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ const displayAllPets = (allPet) => {
                                         d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
                                 </svg>
                             </button>
-                            <button class="btn bg-white text-secondary  border border-secondary">Adopt</button>
-                            <button id='petDetailsBtn' class="btn bg-white text-secondary  border border-secondary" onclick= "petDetails('${pet.petId}')">Details</button>
+                            <button class="btn bg-white text-secondary  border border-secondary" onclick="displayAdopt(event)">Adopt</button>
+                            <button class="btn bg-white text-secondary  border border-secondary" onclick= "petDetails('${pet.petId}')">Details</button>
                         </div>
                     </div>
                 </div>
@@ -54,11 +54,8 @@ const addBeside = (imagePet) => {
 
 
 const petDetails = async (petId) => {
-    console.log(petId)
     const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
-    console.log(response)
     const dataTwo = await response.json();
-    console.log(dataTwo.petData)
     displayPetDetails(dataTwo.petData)
 
 }
@@ -94,6 +91,49 @@ const displayPetDetails = (petData) => {
     `
     modalContainer.appendChild(div);
     my_modal_5.showModal()
+
+}
+
+
+const displayAdopt = (event) => {
+    event.target.onclick = null;
+    event.target.classList="btn text-white bg-gray-300 rounded focus:outline-none disabled"
+
+    document.getElementById("modalContainerTwo").innerHTML = "";
+    const modalContainerTwo = document.getElementById('modalContainerTwo');
+
+    let counter = 3;
+
+    const div = document.createElement('div')
+    div.innerHTML=`
+    <dialog id="my_modal_1" class="modal">
+        <div class="modal-box text-center">
+            <h3 class="text-3xl font-bold">Congratulation</h3>
+            <p class="py-4">The Adoption Process Starts Here!</p>
+            <span id="countdown" class="text-6xl font-bold">${counter}</span>
+        </div>
+    </dialog>
+    
+    `
+    modalContainerTwo.appendChild(div);
+    my_modal_1.showModal(); 
+
+    window.setTimeout(function() {
+        counter--;
+        document.getElementById("countdown").innerText = counter;
+
+        window.setTimeout(function() {
+            counter--;
+            document.getElementById("countdown").innerText = counter;
+
+            window.setTimeout(function() {
+                my_modal_1.close();
+            }, 1000)
+    
+        }, 1000)
+
+    }, 1000)   
+
 
 }
 
